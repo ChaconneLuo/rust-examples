946265a4648fd45bb8644bd52d872d03a0ac67d0
ChaconneLuo <asgardannila@gmail.com>
2023-06-01 11:40:46 +0800

    feat: trait & debug

--- grammar_struct/src/main.rs
+++ grammar_struct/src/main.rs
@@ -4,10 +4,27 @@ struct Rectangle {
     height: u32,
 }
 
+impl std::fmt::Display for Rectangle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "(width: {}, height: {})", self.width, self.height)
+    }
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn idbg<T: std::fmt::Debug + std::fmt::Display>(val: T) -> T {
+    println!("{}", val);
+    println!("{:?}", val);
+    val
+}
+
 fn main() {
-    let rec1 = Rectangle {
+    let mut rec1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("rec1 is {:?}", rec1);
+    rec1 = idbg(rec1);
+    println!("rec1 is {:?}, area is {}", rec1, area(&rec1));
 }
