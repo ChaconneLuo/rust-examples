3bad76bbf47fbf674065bed4e5e4759e3ce42f8a
ChaconneLuo <asgardannila@gmail.com>
2023-06-10 16:24:53 +0800

    feat: args

--- args_command/src/main.rs
+++ args_command/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect::<Vec<String>>();
+    dbg!(&args);
+
+    if args.len() < 2 {
+        println!("Please provide a file name");
+        return;
+    } else {
+        let filename = &args[1];
+        let path = ".\\src\\".to_string() + filename;
+        let contents = fs::read_to_string(&path).expect("Something went wrong reading the file");
+        println!("With text:\n{}", contents);
+    }
 }
