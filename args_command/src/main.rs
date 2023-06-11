4b036a1fb17f687ba5796c34b50abfdca726f320
ChaconneLuo <asgardannila@gmail.com>
2023-06-11 13:27:08 +0800

    chore: use strum

--- args_command/src/main.rs
+++ args_command/src/main.rs
@@ -1,16 +1,40 @@
 use std::env;
 use std::fs;
+use std::str::FromStr;
+use strum_macros::EnumString;
+
 fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
     dbg!(&args);
+    let Config { filename, query } = Config::new(&args);
 
-    if args.len() < 2 {
-        println!("Please provide a file name");
-        return;
-    } else {
-        let filename = &args[1];
-        let path = ".\\src\\".to_string() + filename;
-        let contents = fs::read_to_string(&path).expect("Something went wrong reading the file");
-        println!("With text:\n{}", contents);
+    match QueryOption::from_str(&query).unwrap() {
+        QueryOption::Show => {
+            let contents =
+                fs::read_to_string(filename).expect("Something went wrong reading the file");
+            println!("With text:\n{}", contents);
+        }
+    }
+}
+
+#[derive(Debug, EnumString)]
+enum QueryOption {
+    #[strum(serialize = "show")]
+    Show,
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 2 {
+            panic!("Please provide a file name")
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
     }
 }
