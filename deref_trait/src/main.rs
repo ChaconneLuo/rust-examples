d878d34667d4a8dcaeb1897c24a2b32dd3b7b524
ChaconneLuo <asgardannila@gmail.com>
2023-06-13 14:15:59 +0800

    feat: deref trait

--- deref_trait/src/main.rs
+++ deref_trait/src/main.rs
@@ -1,3 +1,30 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(val: T) -> MyBox<T> {
+        MyBox(val)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = 5;
+    let b = MyBox::new(a);
+
+    assert_eq!(a, *b);
+
+    let str_box = MyBox::new(String::from("ChaconneLuo"));
+    hello(&str_box);
 }
