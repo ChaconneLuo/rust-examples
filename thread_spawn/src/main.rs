902bf2f1ba24b73783ea6ff15745f113d6f5e267
ChaconneLuo <asgardannila@gmail.com>
2023-06-16 20:52:10 +0800

    feat: shared memory

--- thread_spawn/src/main.rs
+++ thread_spawn/src/main.rs
@@ -1,17 +1,31 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let v = vec![1, 2, 3, 4, 5, 5, 4, 3, 2, 1];
+    let data = Arc::new(Mutex::new(0 as usize));
+    let (tx, rx) = mpsc::channel::<usize>();
+    let v: Vec<usize> = vec![1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1];
+    let counter = Arc::clone(&data);
     let handle = thread::spawn(move || {
-        for i in 0..10 {
-            println!("{}", v.get(i).unwrap());
-            thread::sleep(Duration::from_millis(1));
+        let mut mut_counter = counter.lock().unwrap();
+        for i in 0..v.len() {
+            let now_element = *v.get(i).unwrap();
+            *mut_counter = now_element;
+            tx.send(now_element).unwrap();
+            println!("Sent: {}", now_element);
+            thread::sleep(Duration::from_millis(500));
         }
     });
-    for i in 1..5 {
-        println!("main {}", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    thread::spawn(move || loop {
+        let msg = match rx.recv() {
+            Ok(msg) => msg,
+            Err(_) => break,
+        };
+        println!("Got: {}", msg);
+    });
     handle.join().unwrap();
+    println!("{}", *data.lock().unwrap());
 }
