3db505d6fc3690cc5a8bf19fef56b075cf7135c4
ChaconneLuo <asgardannila@gmail.com>
2023-08-05 16:16:00 +0800

    chore: optimize io

--- args_command/src/lib.rs
+++ args_command/src/lib.rs
@@ -15,12 +15,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // if args.len() < 2 {
+        //     return Err("not enough arguments");
+        // }
+        args.next();
+        let query = match args.next() {
+            Some(query) => query,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(filename) => filename,
+            None => return Err("Didn't get a filename"),
+        };
         Ok(Config { query, filename })
     }
 }
--- args_command/src/main.rs
+++ args_command/src/main.rs
@@ -3,9 +3,9 @@ use std::env;
 use args_command::{run, Config};
 
 fn main() {
-    let args: Vec<String> = env::args().collect::<Vec<String>>();
-    dbg!(&args);
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    // let args: Vec<String> = env::args().collect::<Vec<String>>();
+    // dbg!(&args);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
