78f5ea7484a2b4ca179e0d89941eaac10a23d635
ChaconneLuo <asgardannila@gmail.com>
2023-10-07 20:49:03 +0800

    feat: linked_list

--- link_list/src/lib.rs
+++ link_list/src/lib.rs
@@ -1,89 +1,146 @@
 use std::{cell::RefCell, rc::Rc};
+
+type CoreNode<T> = Rc<RefCell<Node<T>>>;
+type LinkNode<T> = Option<CoreNode<T>>;
+
 #[derive(Debug, Clone)]
-pub struct Node<T> {
-    value: Option<T>,
-    next: Option<Rc<RefCell<Node<T>>>>,
+struct Node<T> {
+    value: T,
+    next: LinkNode<T>,
 }
 
-pub trait List<T> {
+pub trait List<T: Copy> {
     fn new() -> Self;
-    fn push_back(&self, value: Option<T>);
+    fn push_back(&mut self, value: T);
     fn len(&self) -> usize;
-    fn unshift(&self, value: Option<T>);
-    fn find(&self, value: Option<T>, eq: fn(args0: &T, args1: &T) -> bool) -> bool;
-    // fn remove(&mut self, value: T);
-    // fn print(&self);
+    fn unshift(&mut self, value: T);
+    fn find(&self, value: T, eq: fn(args0: &T, args1: &T) -> bool) -> bool;
+    fn pop(&mut self) -> Option<T>;
+    fn shift(&mut self) -> Option<T>;
 }
 
 pub struct LinkedList<T> {
-    head: Option<Rc<RefCell<Node<T>>>>,
+    length: i64,
+    head: LinkNode<T>,
+    tail: LinkNode<T>,
 }
 
-impl<T> List<T> for LinkedList<T> {
+impl<T> Node<T> {
+    fn new(value: T) -> CoreNode<T> {
+        Rc::new(RefCell::new(Node { value, next: None }))
+    }
+}
+
+impl<T: Copy> List<T> for LinkedList<T> {
     fn new() -> Self {
         LinkedList {
-            head: Option::Some(Rc::new(RefCell::new(Node {
-                value: Option::None,
-                next: Option::None,
-            }))),
+            length: 0,
+            head: None,
+            tail: None,
         }
     }
 
-    fn push_back(&self, value: Option<T>) {
-        if let Some(ref head) = self.head {
-            let mut current: Rc<RefCell<Node<T>>> = Rc::clone(head);
-            loop {
-                let node_ref = Rc::clone(&current);
-                match &(*node_ref).borrow().next {
-                    Some(node) => {
-                        current = Rc::clone(&node);
-                    }
-                    None => {
-                        break;
-                    }
-                };
-            }
+    fn push_back(&mut self, value: T) {
+        let new_node = Node::new(value);
+        new_node.borrow_mut().next = self.head.take();
 
-            let new_node: Node<T> = Node {
-                value,
-                next: Option::None,
-            };
-            (*current).borrow_mut().next = Option::Some(Rc::new(RefCell::new(new_node)));
+        match self.tail.take() {
+            Some(node) => self.tail = Some(node),
+            None => self.tail = Some(new_node.clone()),
+        }
+
+        self.length += 1;
+        self.head = Some(new_node);
+    }
+
+    fn unshift(&mut self, value: T) {
+        let new_node = Node::new(value);
+
+        match self.tail.take() {
+            Some(node) => {
+                node.borrow_mut().next = Some(new_node.clone());
+            }
+            None => self.head = Some(new_node.clone()),
+        }
+
+        self.length += 1;
+        self.tail = Some(new_node);
+    }
+
+    fn find(&self, value: T, eq: fn(args0: &T, args1: &T) -> bool) -> bool {
+        let mut now_node = self.head.clone();
+        loop {
+            match now_node.take() {
+                Some(node) => {
+                    let res = eq(&value, &node.borrow().value);
+                    if res {
+                        return true;
+                    } else {
+                        now_node = node.borrow().next.clone()
+                    }
+                }
+                None => {
+                    return false;
+                }
+            }
         }
     }
 
     fn len(&self) -> usize {
-        let mut count: usize = 0;
-        if let Some(ref head) = self.head {
-            let mut current: Rc<RefCell<Node<T>>> = Rc::clone(head);
-            loop {
-                let node_ref = Rc::clone(&current);
-                match &(*node_ref).borrow().next {
-                    Some(node) => {
-                        count += 1;
-                        current = Rc::clone(&node);
+        self.length as usize
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        match self.tail.clone() {
+            Some(node) => {
+                if self.length != 1 {
+                    let mut ptr = self.head.clone();
+                    for index in 1..self.length - 1 {
+                        match ptr {
+                            Some(node) => {
+                                ptr = node.borrow().next.clone();
+                            }
+                            None => {}
+                        }
                     }
-                    None => {
-                        break;
+                    match ptr.take() {
+                        Some(node) => node.borrow_mut().next = None,
+                        None => {}
                     }
-                };
+                    self.length -= 1;
+                    self.tail = Some(node.clone());
+                    return Some(node.borrow().value);
+                } else {
+                    self.head = None;
+                    self.tail = None;
+                    self.length -= 1;
+                    return Some(node.borrow().value);
+                }
+            }
+            None => {
+                return None;
             }
         }
-        count
     }
 
-    fn unshift(&self, value: Option<T>) {
-        if let Some(ref head) = self.head {
-            let current: Rc<RefCell<Node<T>>> = Rc::clone(head);
-            let new_node: Node<T> = Node {
-                value,
-                next: (*current).borrow_mut().next.to_owned(),
-            };
-            (*current).borrow_mut().next = Option::Some(Rc::new(RefCell::new(new_node)));
+    fn shift(&mut self) -> Option<T> {
+        match self.head.clone() {
+            Some(node) => {
+                match &node.clone().borrow().next {
+                    Some(next_node) => {
+                        self.head = Some(next_node.clone());
+                    }
+                    None => {
+                        self.head = None;
+                        self.tail = None;
+                    }
+                }
+                self.length -= 1;
+                return Some(node.borrow().value);
+            }
+            None => {
+                return None;
+            }
         }
-    }
-
-    fn find(&self, value: Option<T>, eq: fn(args0: &T, args1: &T) -> bool) -> bool {
-        true
     }
 }
--- link_list/src/main.rs
+++ link_list/src/main.rs
@@ -1,13 +1,14 @@
 use link_list::{LinkedList, List};
 
 fn main() {
-    let link_list = LinkedList::<i32>::new();
-    link_list.unshift(Option::Some(66));
-    link_list.push_back(Option::Some(66));
-    link_list.push_back(Option::Some(99));
+    let mut link_list = LinkedList::<i32>::new();
+    link_list.unshift(66);
+    link_list.push_back(66);
+    link_list.push_back(99);
     println!("link_list length: {}", link_list.len());
-    println!(
-        "link_list has 99: {}",
-        link_list.find(Some(99), |a, b| a == b)
-    );
+    println!("link_list has 99: {}", link_list.find(99, |a, b| a == b));
+    println!("link_list shift: {}", link_list.shift().unwrap());
+    println!("link_list shift: {}", link_list.shift().unwrap());
+    println!("link_list pop: {}", link_list.pop().unwrap());
+    println!("link_list pop: {}", link_list.len());
 }
