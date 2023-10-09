42dd6a55a9a82af22e191adfe8760428b7a5abd5
ChaconneLuo <asgardannila@gmail.com>
2023-10-09 18:28:22 +0800

    chore: edit test

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,19 +1,35 @@
-use linked_list::{LinkedList, List};
+use std::{collections::LinkedList, time::Instant};
+
+use linked_list::List;
 
 fn main() {
-    let mut link_list = LinkedList::<i32>::new();
-    link_list.push_back(66);
-    link_list.push_back(66);
-    link_list.unshift(99);
-    println!("link_list length: {}", link_list.len());
-    // let mut iter = link_list.into_iter();
-    // iter.next();
-    // println!("link_list has 99: {}", link_list.find(99, |a, b| a == b));
-    // println!("link_list shift: {}", link_list.shift().unwrap());
-    // println!("link_list shift: {}", link_list.shift().unwrap());
-    println!("link_list pop: {}", link_list.pop().unwrap());
-    println!("link_list pop: {}", link_list.pop().unwrap());
-    // println!("link_list ele:{}", iter.next().unwrap());
-    // println!("link_list ele:{}", iter.next().unwrap());
-    // println!("link_list ele:{}", iter.next().unwrap());
+    let mut own_link_list = linked_list::LinkedList::<i32>::new();
+    let mut std_link_list: LinkedList<i32> = LinkedList::<i32>::new();
+
+    let test_cnt = 1000;
+
+    let mut start_time = Instant::now();
+    for i in 0..test_cnt {
+        own_link_list.push_back(i);
+        own_link_list.unshift(i);
+    }
+    for _i in 0..test_cnt {
+        own_link_list.pop();
+        own_link_list.shift();
+    }
+    let mut end_time = Instant::now();
+
+    println!("own:{:?}", end_time - start_time);
+
+    start_time = Instant::now();
+    for i in 0..test_cnt {
+        std_link_list.push_back(i);
+        std_link_list.push_front(i);
+    }
+    for _i in 0..test_cnt {
+        std_link_list.pop_back();
+        std_link_list.pop_front();
+    }
+    end_time = Instant::now();
+    println!("std:{:?}", end_time - start_time);
 }
